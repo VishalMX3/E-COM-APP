981b6aa621e4bb330efc2556d5f42fc7398fc45a
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-01 22:08:19 +0530

    Announcement Banner Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Products from "../components/Products";
+import Announcement from "../components/Announcement";
 
 const Home = () => {
   return (
     <div>
+      <Announcement />
       <Navbar />
       <Products />
     </div>
