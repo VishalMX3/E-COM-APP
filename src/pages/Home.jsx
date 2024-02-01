7cefd561d217088b9a52e8d57f250ec242ea1be5
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-01 22:14:24 +0530

    Slider Section created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Products from "../components/Products";
 import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
       <Announcement />
       <Navbar />
+      <Slider />
       <Products />
     </div>
   );
